3a5eb949fb7c3d8846b0101731bfed3be545849d
Piotr Leniec <leniec.piotr@gmail.com>
2017-11-19 15:42:11 +0100

    dev config of the frontend

---
+++ client/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1 id="message">{{ message }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return { message: 'Hello!' }
+  }
+}
+</script>
+
+<style>
+#message {
+  color: blue;
+}
+</style>
---
+++ webpack.config.dev.js
@@ -0,0 +1,17 @@
+const path = require('path')
+
+module.exports = {
+  entry: path.join(__dirname, 'client', 'index.js'),
+  output: {
+    publicPath: '/assets/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.vue/,
+        loader: 'vue-loader'
+      }
+    ]
+  }
+}
