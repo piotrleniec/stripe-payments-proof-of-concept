f3b3fc2b2a92a6245954d9647465b69580779d4a
Piotr Leniec <leniec.piotr@gmail.com>
2017-11-19 16:10:26 +0100

    prod config of the frontend

--- server.rb
+++ server.rb
@@ -2,5 +2,5 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 
 get '/' do
-  send_file File.join(__dir__, 'public', 'index.html')
+  erb :index, locals: { env: ENV['APP_ENV'] }
 end
---
+++ webpack.config.prod.js
@@ -0,0 +1,25 @@
+const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+module.exports = {
+  entry: path.join(__dirname, 'client', 'index.js'),
+  output: {
+    path: path.join(__dirname, 'public', 'assets'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.vue/,
+        loader: 'vue-loader',
+        options: { extractCSS: true }
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: 'bundle.css',
+      allChunks: false
+    })
+  ]
+}
