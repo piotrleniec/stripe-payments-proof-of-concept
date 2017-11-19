2c418ffd1b8269934ef4967b9c531cf6c149a8bc
Piotr Leniec <leniec.piotr@gmail.com>
2017-11-19 19:40:50 +0100

    CreditCardForm styling

--- client/App.vue
+++ client/App.vue
@@ -1,12 +1,23 @@
 <template>
-  <h1 id="message">{{ message }}</h1>
+  <div>
+    <h1 id="message">{{ message }}</h1>
+
+    <div class="col-xs-offset-4 col-xs-4">
+      <credit-card-form />
+    </div>
+  </div>
 </template>
 
 <script>
+import CreditCardForm from './CreditCardForm.vue'
+
 export default {
   name: 'app',
   data() {
     return { message: 'Hello!' }
+  },
+  components: {
+    'credit-card-form': CreditCardForm
   }
 }
 </script>
---
+++ client/CreditCardForm.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="panel panel-primary">
+    <div class="panel-body">
+      <div class="form-group">
+        <label for="number">Card Number</label>
+        <input id="number" class="form-control" v-model="number" />
+      </div>
+      <div class="form-group">
+        <div class="row">
+          <div class="col-xs-8">
+            <label for="cvc">Expiration Date</label>
+            <input id="cvc" class="form-control" v-model="expDate" />
+          </div>
+          <div class="col-xs-4">
+            <label for="exp-month">Security Code</label>
+            <input id="exp-month" class="form-control" v-model="cvc" />
+          </div>
+        </div>
+      </div>
+      <button class="btn btn-primary">Pay</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'credit-card-form',
+  data() {
+    return {
+      number: null,
+      expDate: null,
+      cvc: null
+    }
+  }
+}
+</script>
